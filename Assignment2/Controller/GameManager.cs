e608442d8d7bef8bbe4cb9a6a1931b8a34eec790
Vilmer Levin <vilmerlevin@gmail.com>
2025-11-07 17:56:35 +0100

    safafsaf

--- Assignment2/Controller/GameManager.cs
+++ Assignment2/Controller/GameManager.cs
@@ -42,7 +42,7 @@ namespace Assignment2.Controller
                     
                     Move moveMade = await CurrentPlayer.RequestMove(validMoves);
                     board.MakeMove(moveMade);
-                    gg.Token(Convert.ToString(moveMade.Player), moveMade.row, moveMade.col );
+                    gg.Token(Convert.ToString(moveMade.Player), moveMade.row +1, moveMade.col +1);
                     Position chosenMove = new Position { row = coordinates[0], col = coordinates[1] };
                     SwitchPlayer();
                     wasMoveMade = true;
